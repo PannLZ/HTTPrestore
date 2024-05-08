7a88ea79c41bc5d9dca76d621c5e69ed39026223
PhilThrive <11871372+pannlz@user.noreply.gitee.com>
2024-05-08 20:19:42 +0800

    第一次提交

---
+++ HTTPrestore/mian.cpp
@@ -0,0 +1,18 @@
+//#define HAVE_REMOTE
+//#define WPCAP
+#define WIN32
+
+#include "pcap.h" 
+#include <winsock2.h>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+#pragma comment(lib, "wpcap.lib")
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+
+	return 0;
+}
